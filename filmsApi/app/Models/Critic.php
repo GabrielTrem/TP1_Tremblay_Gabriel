63d61a1944a041611cc01e84fa223d86ff2abd49
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-02-26 09:27:54 -0500

    Critic model finished

--- filmsApi/app/Models/Critic.php
+++ filmsApi/app/Models/Critic.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Critic extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'film_id',
+        'score',
+        'comment'
+    ];
+
     public function user(){
         return $this->belongsTo('App\Models\User');
     }
