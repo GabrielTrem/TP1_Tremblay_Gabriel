1bbe1b64e5257057836e819d186decead4f95f27
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-02-26 11:14:30 -0500

    factory created for critic finished

---
+++ filmsApi/database/factories/CriticFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Critic>
+ */
+
+use Faker\Factory as Faker;
+use App\Models\User;
+use App\Models\Film;
+
+class CriticFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = Faker::create();
+        return [
+            'film_id'=>Film::all()->random()->id, //trouver sur https://stackoverflow.com/questions/44102483/in-laravel-how-do-i-retrieve-a-random-user-id-from-the-users-table-for-model-fa
+            'score' =>$faker->randomFloat(1, 0, 10),
+            'comment' =>$faker->pragraph(2)
+        ];
+    }
+}
