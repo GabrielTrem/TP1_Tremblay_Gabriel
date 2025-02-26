b59dfeadee1cf75d882acd52425a0d3f6984b15e
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-02-26 09:36:41 -0500

    Actor model finished

--- filmsApi/app/Models/Actor.php
+++ filmsApi/app/Models/Actor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actor extends Model
 {
-    //
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'birthdate'
+    ];
 }
