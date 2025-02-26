7d0dd1dd791eb881e0e3b9f5de63942628d16b7d
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-02-26 10:13:05 -0500

    seeder for Language finished

---
+++ filmsApi/database/seeders/LanguageSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sql = file_get_contents(database_path() . '/seeders/languages.sql');
+        DB::statement($sql);
+    }
+}
