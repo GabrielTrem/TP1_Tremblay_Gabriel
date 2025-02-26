993834610e980f710cb483d818f1dd053366915b
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-02-26 10:17:18 -0500

    seeder for actor_film finished

---
+++ filmsApi/database/seeders/ActorFilmSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActorFilmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sql = file_get_contents(database_path() . '/seeders/actor_film.sql');
+        DB::statement($sql);
+    }
+}
--- filmsApi/database/seeders/DatabaseSeeder.php
+++ filmsApi/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LanguageSeeder::class,
             FilmSeeder::class,
-            ActorSeeder::class
+            ActorSeeder::class,
+            ActorFilmSeeder::class
         ]);
     }
 }
