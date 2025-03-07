eee338f4e958a84d5bf0e377812c350f2409dab6
Gabriel Tremblay <168035349+GabrielTrem@users.noreply.github.com>
2025-03-07 09:33:50 -0500

    Controller UserController finished
    for route 4 et 5

--- filmsApi/routes/api.php
+++ filmsApi/routes/api.php
@@ -10,8 +10,8 @@ Route::get('films/{id}/actors', 'App\Http\Controllers\FilmActorController@index'
 Route::get('films/{id}/critics', 'App\Http\Controllers\FilmCriticController@index');
 Route::get('films/{id}/average-score', 'App\Http\Controllers\FilmCriticController@averageScore');
 
-// Route::post('users', 'App\Http\Controllers\UserController@store');
-// Route::put('users/{id}', 'App\Http\Controllers\UserController@update');
+Route::post('users', 'App\Http\Controllers\UserController@store');
+Route::put('users/{id}', 'App\Http\Controllers\UserController@update');
 // Route::get('users/{id}/favorite-language', 'App\Http\Controllers\UserLanguageController@favoriteLanguage');
 
 Route::delete('critics/{id}', 'App\Http\Controllers\CriticController@destroy');
